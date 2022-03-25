ae37b0011a5cadc36385514a2b1f1680a021400d
Rachael Berecka <rachael.berecka@hashicorp.com>
2022-03-25 15:47:42 -0700

    asdf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 
    
 output "print-them-all" {
- value = "${var.word_count}, ${var.sleep_seconds}, ${var.secret_key}, ${var.aws_region_no_default}, ${var.aws_bucket_name}, ${var.cpu_capacity} , ${var.admin_user}"
+ value = "${var.word_count}, ${var.sleep_seconds}, can't print the secret key value, ${var.aws_region_no_default}, ${var.aws_bucket_name}, ${var.cpu_capacity} , ${var.admin_user}"
 }
