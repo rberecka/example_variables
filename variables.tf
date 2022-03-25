56bee62ecac4f33c46125cf6ddb16db2d815939d
Rachael Berecka <rachael.berecka@hashicorp.com>
2022-03-25 15:34:42 -0700

    outputs

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "word_count" {
+  type = number
+  description = "A locally executed script will wait this amout of time between retries"
+}
+
 variable "sleep_seconds" {
   type = number
   description = "A locally executed script will wait this amout of time between retries"
