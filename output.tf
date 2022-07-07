e3e923af7b17332781767e048ab5a99069393875
rberecka <rachael.berecka@hashicorp.com>
2022-07-07 08:50:43 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,11 +4,9 @@ output "print-them-all" {
  value = <<EOT
             Word Count: ${var.word_count}
             Sleep Seconds: ${var.sleep_seconds}
-            TF won't let you print your sensitive variable, 
-            Could not print a list for Availability Zones,
+            AWS Region With Default: ${var.aws_region_default}
             AWS Region No Default: ${var.aws_region_no_default}
-            AWS Bucket Name${var.aws_bucket_name}
-            CPU Capacity, try bigger than 10${var.cpu_capacity},
+            CPU Capacity: ${var.cpu_capacity},
             Admin User ${var.admin_user["name"]}
             Admin User Email${var.admin_user["email"]}
           EOT
